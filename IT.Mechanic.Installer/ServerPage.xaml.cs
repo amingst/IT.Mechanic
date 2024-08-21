a22060da6e93abf3a4ecf09347aeb239cbb5e447
amingsttc <amingst97@gmail.com>
2024-08-21 18:48:52 -0400

    add fields to server page

--- IT.Mechanic.Installer/ServerPage.xaml.cs
+++ IT.Mechanic.Installer/ServerPage.xaml.cs
@@ -5,6 +5,7 @@ namespace IT.Mechanic.Installer;
 public partial class ServerPage : ContentPage
 {
 	public ServerModel serverModel { get; set; }
+	public bool IsExpertMode { get; set; } = false;
 	public ServerPage()
 	{
 		InitializeComponent();
