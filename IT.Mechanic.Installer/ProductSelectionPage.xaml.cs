69e9cff6842f467d564edab94651dc9dbbc1c0d9
Andrew Mingst <amingst97@gmail.com>
2024-08-22 00:31:44 -0400

    fix typo

--- IT.Mechanic.Installer/ProductSelectionPage.xaml.cs
+++ IT.Mechanic.Installer/ProductSelectionPage.xaml.cs
@@ -4,11 +4,8 @@ namespace IT.Mechanic.Installer;
 
 public partial class ProductSelectionPage : ContentPage
 {
-	//public ProductSelectionModel productSelection { get; set; }
 	public ProductSelectionPage()
 	{
 		InitializeComponent();
-		//productSelection = new ProductSelectionModel();
-		//BindingContext = productSelection;
 	}
 }
