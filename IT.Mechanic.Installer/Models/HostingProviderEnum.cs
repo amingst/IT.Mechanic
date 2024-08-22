6d444089dd9f672be1c05b68460ebc566ecfb580
amingsttc <amingst97@gmail.com>
2024-08-22 16:34:05 -0400

    separate enums for data binding

---
+++ IT.Mechanic.Installer/Models/HostingProviderEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.Mechanic.Installer.Models
+{
+    public enum HostingProviderEnum
+    {
+        Expertmode,
+        Rumble,
+        Digitalocean,
+        Azure,
+        AWS,
+        GCP,
+        Invertedtech,
+    }
+}
