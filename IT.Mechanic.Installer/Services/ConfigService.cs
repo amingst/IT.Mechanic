0b7e50c56e2795c7423802af4e446ea2a75a3b75
Andrew Mingst <amingst97@gmail.com>
2024-08-22 02:22:23 -0400

    Add config service

---
+++ IT.Mechanic.Installer/Services/ConfigService.cs
@@ -0,0 +1,19 @@
+﻿using IT.Mechanic.Models.Configuration;
+using IT.Mechanic.Models.Configuration.Credentials;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.Mechanic.Installer.Services
+{
+    public class ConfigService : IConfigService
+    {
+        public Guid SiteId { get; set; } = Guid.Empty;
+        public DNSModel DNS { get; set; } = new();
+        public ServerModel Server { get; set; } = new();
+        public List<CredentialsBase> Credentials { get; set; } = new();
+        public ProductSelectionModel ProductSelection { get; set; } = new();
+    }
+}
