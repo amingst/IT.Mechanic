43cd40916211d2056a688221782b9aa0d3309f11
Andrew Mingst <amingst97@gmail.com>
2024-08-22 00:29:54 -0400

    Bind Product Page Data To MainModel on AppShell

--- IT.Mechanic.Installer/AppShell.xaml.cs
+++ IT.Mechanic.Installer/AppShell.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace IT.Mechanic.Installer
+﻿using IT.Mechanic.Models.Configuration;
+
+namespace IT.Mechanic.Installer
 {
     public partial class AppShell : Shell
     {
+        public MainModel MainModel { get; set; }
         public AppShell()
         {
             InitializeComponent();
+            MainModel = new MainModel();
+            BindingContext = MainModel;
         }
     }
 }
--- IT.Mechanic.Installer/ProductSelectionPage.xaml.cs
+++ IT.Mechanic.Installer/ProductSelectionPage.xaml.cs
@@ -4,11 +4,11 @@ namespace IT.Mechanic.Installer;
 
 public partial class ProductSelectionPage : ContentPage
 {
-	public ProductSelectionModel productSelection { get; set; }
+	//public ProductSelectionModel productSelection { get; set; }
 	public ProductSelectionPage()
 	{
 		InitializeComponent();
-		productSelection = new ProductSelectionModel();
-		BindingContext = productSelection;
+		//productSelection = new ProductSelectionModel();
+		//BindingContext = productSelection;
 	}
 }
