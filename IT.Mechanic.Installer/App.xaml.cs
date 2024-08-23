1dcc644e7f1d2516293991f452b054c003e7d66d
Andrew Mingst <amingst97@gmail.com>
2024-08-23 02:15:52 -0400

    set window size, comment out entry on Server Page

--- IT.Mechanic.Installer/App.xaml.cs
+++ IT.Mechanic.Installer/App.xaml.cs
@@ -11,5 +11,18 @@ namespace IT.Mechanic.Installer
 
             MainPage = new AppShell(_configService);
         }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 800;
+            const int newHeight = 600;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
     }
 }
