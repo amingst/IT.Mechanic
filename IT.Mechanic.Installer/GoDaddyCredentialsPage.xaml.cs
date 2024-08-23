fa4ecc6020df6e3c05e8acc281663b8912bc62db
Andrew Mingst <amingst97@gmail.com>
2024-08-23 02:48:28 -0400

    Page for godaddy credentials entry

--- IT.Mechanic.Installer/GoDaddyCredentialsPage.xaml.cs
+++ IT.Mechanic.Installer/GoDaddyCredentialsPage.xaml.cs
@@ -1,20 +1,27 @@
 using IT.Mechanic.Installer.Services;
+using IT.Mechanic.Models.Configuration.Credentials;
 
 namespace IT.Mechanic.Installer;
 
 public partial class GoDaddyCredentialsPage : ContentPage
 {
 	private ConfigService _configService {  get; set; }
+    public GodaddyModel GoDaddyCredentials { get; set; } = new();
 	public GoDaddyCredentialsPage()
 	{
 		InitializeComponent();
 		_configService = App.Current.Handler.MauiContext.Services.GetService<ConfigService>();
-		BindingContext = _configService.Model;
+        BindingContext = GoDaddyCredentials;
 	}
 
     public async void OnNextClicked(object sender, EventArgs e)
     {
-        await AppShell.Current.GoToAsync("//Server");
+        BindingContext = _configService.Model;
+        if (BindingContext != null)
+        {
+            _configService.Model.Credentials.Add(GoDaddyCredentials);
+            await AppShell.Current.GoToAsync("//Server");
+        }
     }
 
     public async void OnBackClicked(object sender, EventArgs e)
