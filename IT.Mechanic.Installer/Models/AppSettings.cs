b5241babf3e700ae3a96eb424a580dace4c55c07
Andrew Mingst <amingst97@gmail.com>
2024-08-24 07:52:04 -0400

    add model for settings

---
+++ IT.Mechanic.Installer/Models/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.Mechanic.Installer.Models
+{
+    public class AppSettings
+    {
+        public readonly string ProfilesFileName = "mechanic.json";
+        public string ProfilesDownloadFilePath { get; private set; } = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        public string MechanicCliPath { get; private set; } = String.Empty;
+    }
+}
