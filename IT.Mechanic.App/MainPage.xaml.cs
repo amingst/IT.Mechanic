0122647fd84175693be894df421d45562b9d93db
Andrew Mingst <amingst97@gmail.com>
2024-08-28 23:41:08 -0400

    init

---
+++ IT.Mechanic.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace IT.Mechanic.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ IT.Mechanic.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IT.Mechanic.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
