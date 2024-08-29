daec7d0fdb067d97ed443a3e1c597a22740d298f
Andrew Mingst <amingst97@gmail.com>
2024-08-28 23:52:31 -0400

    add mudblazor

--- IT.Mechanic.App/MauiProgram.cs
+++ IT.Mechanic.App/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace IT.Mechanic.App
 {
@@ -15,6 +16,7 @@ namespace IT.Mechanic.App
                 });
 
             builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddMudServices();
 
 #if DEBUG
     		builder.Services.AddBlazorWebViewDeveloperTools();
