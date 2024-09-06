5737f6131e23c47c7ae0c87b46b47ae08d15c057
amingsttc <amingst97@gmail.com>
2024-09-06 17:47:43 -0400

    add json helpers

---
+++ IT.Mechanic.CLI/Helpers/JSONHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using IT.Mechanic.Models.Configuration;
+
+namespace IT.Mechanic.CLI.Helpers
+{
+    public class JSONHelpers
+    {
+        private static JsonSerializerOptions _jsonSerializerOpts = new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) },
+        };
+
+        public static MainModel ReadProfile(string profileId)
+        {
+            return new MainModel { };
+        }
+    }
+}
--- IT.Mechanic.CLI/Program.cs
+++ IT.Mechanic.CLI/Program.cs
@@ -4,7 +4,11 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        if (args.Any(arg => arg.Equals("-h")) || args.Any(arg => arg.Equals("--help")))
+        if (
+            args.Any(arg => arg.Equals("-h"))
+            || args.Any(arg => arg.Equals("--help"))
+            || args.Count() == 0
+        )
         {
             WriteHelpMessage();
         }
@@ -15,8 +19,15 @@ public class Program
         Console.WriteLine("================================");
         Console.WriteLine("Welcome To Inverted Tech Mechanic:");
         Console.WriteLine("================================");
+        Console.WriteLine("Usage:");
+        Console.WriteLine("mechanic [PATH-TO-MECHANIC-PROFILES-DIR] [Command]");
+        Console.WriteLine("mechanic -h/--help => Displays Helpful Information");
         Console.WriteLine("Commands:");
-        Console.WriteLine("-h/--help: Displays Helpful Information");
+        Console.WriteLine("start => Starts a Stopped Server");
+        Console.WriteLine("stop => Stops a Stopped Server");
+        Console.WriteLine("backup => Generates A Server Backup");
+        Console.WriteLine("migrate => Migrates A Server");
+        Console.WriteLine("delete => Stops a Stopped Server");
         Console.Read();
     }
 }
